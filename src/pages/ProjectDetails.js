4d11e70e0a1db8aa61a1e41b7dc732af645707de
MomenSherif <momensherif.2019@gmail.com>
2020-04-27 20:29:05 +0200

    🐛 BUG:  Renamed component used in Project Details Page

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -8,58 +8,58 @@ import BusinessIcon from '@material-ui/icons/Business';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import ScheduleIcon from '@material-ui/icons/Schedule';
 import TimelineIcon from '@material-ui/icons/Timeline';
-import ProjectCard from '../components/ProjectCard/ProjectCard';
+import ProjectDetailsCard from '../components/ProjectDetailsCard/ProjectDetailsCard';
 import moment from 'moment';
 
 const ProjectDetails = () => {
   return (
     <Container>
-      <Typography variant="h2" gutterBottom>
+      <Typography variant='h2' gutterBottom>
         Project Overview
       </Typography>
-      <Grid container justify="space-evenly" spacing={6}>
+      <Grid container justify='space-evenly' spacing={6}>
         <Grid item xs={12} md={6}>
-          <ProjectCard
-            title="Project Name"
-            description="Project Management Dashboard"
+          <ProjectDetailsCard
+            title='Project Name'
+            description='Project Management Dashboard'
           >
-            <CreateIcon color="secondary" fontSize="large"></CreateIcon>
-          </ProjectCard>
+            <CreateIcon color='secondary' fontSize='large'></CreateIcon>
+          </ProjectDetailsCard>
         </Grid>
         <Grid item xs={12} md={6}>
-          <ProjectCard title="Organization/Company" description="ITI">
-            <BusinessIcon color="secondary" fontSize="large" />
-          </ProjectCard>
+          <ProjectDetailsCard title='Organization/Company' description='ITI'>
+            <BusinessIcon color='secondary' fontSize='large' />
+          </ProjectDetailsCard>
         </Grid>
         <Grid item xs={12}>
-          <ProjectCard
-            title="Description"
-            description="balalhsdhjsdsdshfsfjhffhffhffgjhfvf"
+          <ProjectDetailsCard
+            title='Description'
+            description='balalhsdhjsdsdshfsfjhffhffhffgjhfvf'
           >
-            <AssignmentIcon color="secondary" fontSize="large" />
-          </ProjectCard>
+            <AssignmentIcon color='secondary' fontSize='large' />
+          </ProjectDetailsCard>
         </Grid>
         <Grid item xs={3}>
-          <ProjectCard title="Budget" description="200000$">
-            <MonetizationOnIcon color="secondary" fontSize="large" />
-          </ProjectCard>
+          <ProjectDetailsCard title='Budget' description='200000$'>
+            <MonetizationOnIcon color='secondary' fontSize='large' />
+          </ProjectDetailsCard>
         </Grid>
         <Grid item xs={3}>
-          <ProjectCard title="State" description="In Progress">
-            <TimelineIcon color="secondary" fontSize="large" />
-          </ProjectCard>
+          <ProjectDetailsCard title='State' description='In Progress'>
+            <TimelineIcon color='secondary' fontSize='large' />
+          </ProjectDetailsCard>
         </Grid>
         <Grid item xs={3}>
-          <ProjectCard
-            title="Deadline"
+          <ProjectDetailsCard
+            title='Deadline'
             description={
               <time dateTime={moment(new Date()).format()}>
                 {moment(new Date()).format('D MMMM YYYY')}
               </time>
             }
           >
-            <ScheduleIcon color="secondary" fontSize="large" />
-          </ProjectCard>
+            <ScheduleIcon color='secondary' fontSize='large' />
+          </ProjectDetailsCard>
         </Grid>
       </Grid>
     </Container>
