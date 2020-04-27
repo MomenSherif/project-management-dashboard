770e2a950b8fb5a71a8e9bec9016853382caf78c
Cady Emad <cady.emad@stud.fci-cu.edu.eg>
2020-04-27 12:55:12 +0200

    🆕 NEW: Team members card (#2)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Fragment } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Teams from './pages/Teams';
 
+import TeamDetails from './pages/TeamDetails';
+
 function App() {
   return (
     <Fragment>
       <ToastContainer />
       <Teams></Teams>
+      <TeamDetails />
     </Fragment>
   );
 }
