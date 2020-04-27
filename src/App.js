3f319318e99aec7b787ccc398a2df21206cb36c8
MomenSherif <momensherif.2019@gmail.com>
2020-04-27 19:56:10 +0200

    ✏️ TYPO:  Project Details Card Renamed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { Fragment } from 'react';
 import { ToastContainer } from 'react-toastify';
-import Teams from './pages/Teams';
-import ProjectDetails from './pages/ProjectDetails';
-
-import TeamDetails from './pages/TeamDetails';
 
 function App() {
   return (
     <Fragment>
-
-      <ProjectDetails></ProjectDetails>
       <ToastContainer />
-      <Teams></Teams>
-      <TeamDetails />
     </Fragment>
   );
 }
