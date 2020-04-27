f58790233ed60bc1a6c049040e4bc403f971e51a
Menna <menna.elnokally@gmail.com>
2020-04-27 17:19:59 +0200

    Design Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Teams from './pages/Teams';
+import ProjectDetails from './pages/ProjectDetails';
 
 function App() {
-  return <Teams></Teams>;
+  return (
+    <Fragment>
+      <Teams></Teams>
+      <ProjectDetails></ProjectDetails>
+    </Fragment>
+  );
 }
 
 export default App;
