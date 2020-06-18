3251a039583cb434b31c3758173a75c9893bd302
Menna <menna.elnokally@gmail.com>
2020-06-19 01:47:08 +0200

    🎨 IMPROVE: Task description and deadline

--- src/pages/UserDetails.js
+++ src/pages/UserDetails.js
@@ -7,7 +7,11 @@ import Grid from '@material-ui/core/Grid';
 import Switch from '@material-ui/core/Switch';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import GroupIcon from '@material-ui/icons/Group';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import ScheduleIcon from '@material-ui/icons/Schedule';
@@ -174,7 +178,30 @@ const UserDetails = ({ match }) => {
                     spacing={3}
                   >
                     <Grid item>
-                      <Typography variant='h4'>{task.title}</Typography>
+                      <ExpansionPanel>
+                        <ExpansionPanelSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls='panel1a-content'
+                          id='panel1a-header'
+                        >
+                          <Typography variant='h4'>{task.title}</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                          <Typography variant='body1' gutterBottom>
+                            {task.description}
+                          </Typography>
+                        </ExpansionPanelDetails>
+                        <ExpansionPanelDetails>
+                          <Typography variant='subtitle2' gutterBottom>
+                            DeadLine:{' '}
+                            <time
+                              date={moment(task.deadLine).format('YYYY-MM-DD')}
+                            >
+                              {moment(task.deadLine).format('D MMMM YYYY')}
+                            </time>
+                          </Typography>
+                        </ExpansionPanelDetails>
+                      </ExpansionPanel>
                     </Grid>
                     {switchState != false && (
                       <Grid item>
